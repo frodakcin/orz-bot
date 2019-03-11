d5fbe4c2c7599a90d3a6f389a620b472ffa8e5c8
Siyong <huangsy222@gmail.com>
2019-03-10 18:33:25 -0700

    Update mute.py

--- mute.py
+++ mute.py
@@ -12,21 +12,39 @@ class Muted:
 		self.user = user
 		self.endOfMute = when
 	
-	def operator __lt__ (self, other):
+	def __lt__ (self, other):
 		if(endOfMute != other.endOfMute):
 			return endOfMute < other.endOfMute
 		return self.user.id < other.user.id
 	
-	def operator __gt__ (self, other):
+	def __gt__ (self, other):
 		if(endOfMute != other.endOfMute):
 			return endOfMute > other.endOfMute
 		return self.user.id > other.user.id
 	
-	def increase_mute_length(x, penalty):
+	def increase_mute_length(penalty):
 		endOfMute = endOfMute + penalty;
+	
+	def toJSON(self):
+		return json.dumps(self, default=lambda o : o.__dict__, sort_keys=True, indent=4)
+
+def encode_datetime(dt):
+	if isinstance(dt, datetime.datetime):
+		return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+	else:
+		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_datetime")
+def encode_Muted(x):
+	if isinstance(x, Muted):
+		return (x.user, encode_datetime(x.endOfMute))
+	else:
+		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_Muted")
 
 
 def save():
 	with open(MuteDataFilePath, "w") as write_file:
-		json.dump(muteList, write_file)
+		for x in muteList:
+			json.dump(x, write_file, default=encode_Muted, sort_keys=True, indent=2)
+
+muteList.append(Muted(1234, datetime.datetime(2009, 3, 10).now()))
+save()
 
