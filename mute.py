4228e3f7e648482c685bfbb94e8008505475b019
Siyong <huangsy222@gmail.com>
2019-03-12 14:55:48 -0700

    Update mute.py

--- mute.py
+++ mute.py
@@ -24,32 +24,6 @@ class Muted:
 	
 	def increase_mute_length(penalty):
 		endOfMute = endOfMute + penalty;
-	
-	def toJSON(self):
-		return json.dumps(self, default=lambda o : o.__dict__, sort_keys=True, indent=4)
-
-def encode_datetime(dt):
-	if isinstance(dt, datetime.datetime):
-		return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
-	else:
-		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_datetime")
-		
-def encode_Muted(x):
-	if isinstance(x, Muted):
-		return (x.user, encode_datetime(x.endOfMute))
-	else:
-		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_Muted")
-		
-def decode_Muted(x):
-	return Muted(x[0], datetime.datetime(x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
-
-def save():
-	with open(MuteDataFilePath, "w") as write_file:
-		json.dump(muteList, write_file, default=encode_Muted, sort_keys=True, indent=2)
-
-def load():
-	with open(MuteDataFilePath, "r") as read_file:
-		muteList = json.load(read_file, object_hook=decode_Muted)
 
 async def mute(bot, message):
 	try:
