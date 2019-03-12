c8b04af482b5f6716d0fd5a0ecdcef8e0679da8c
Siyong <huangsy222@gmail.com>
2019-03-12 15:16:51 -0700

    add muteData

--- muteData.py
+++ muteData.py
@@ -4,52 +4,38 @@ import json
 import discord
 from discord import *
 
+import mute
+from mute import Muted
+
 MuteDataFilePath = "mute.json"
 
 muteList = []
-class Muted:
-	def __init__(self, user, when):
-		self.user = user
-		self.endOfMute = when
-	
-	def __lt__ (self, other):
-		if(endOfMute != other.endOfMute):
-			return endOfMute < other.endOfMute
-		return self.user.id < other.user.id
-	
-	def __gt__ (self, other):
-		if(endOfMute != other.endOfMute):
-			return endOfMute > other.endOfMute
-		return self.user.id > other.user.id
-	
-	def increase_mute_length(penalty):
-		endOfMute = endOfMute + penalty;
 
 def encode_datetime(dt):
-	if isinstance(dt, datetime.datetime):
+	if isinstance(dt, datetime):
 		return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
 	else:
 		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_datetime")
 		
 def encode_Muted(x):
 	if isinstance(x, Muted):
-		return (x.user, encode_datetime(x.endOfMute))
+		return {"user": (x.user), "when": encode_datetime(x.endOfMute)}
 	else:
 		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_Muted")
-		
+
+def get_datetime(x):
+	return datetime(x[0], x[1], x[2], x[3], x[4], x[5])
+
 def decode_Muted(x):
-	return Muted(x[0], datetime.datetime(x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
+	return Muted(x["user"], get_datetime(x["when"]))
 
 def save():
 	with open(MuteDataFilePath, "w") as write_file:
 		json.dump(muteList, write_file, default=encode_Muted, sort_keys=True, indent=2)
 
 def load():
+	global muteList
 	with open(MuteDataFilePath, "r") as read_file:
 		muteList = json.load(read_file, object_hook=decode_Muted)
+		print(len(muteList))
 
-muteList = [];
-muteList.append(Muted(0, datetime.now());
-muteList.append(Muted(1, datetime.now());
-muteList.append(Muted(2, datetime.now());
-muteList.append(Muted(3, datetime.now());
