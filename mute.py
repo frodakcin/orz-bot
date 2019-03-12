196fcea091e1d68430a9ccf91c880e99d2edef1b
Blastman <48421287+aav123@users.noreply.github.com>
2019-03-11 20:27:06 -0400

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,6 +1,6 @@
 import datetime
+from datetime import timedelta
 import json
-
 import discord
 from discord import *
 
@@ -33,23 +33,61 @@ def encode_datetime(dt):
 		return (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
 	else:
 		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_datetime")
+		
 def encode_Muted(x):
 	if isinstance(x, Muted):
 		return (x.user, encode_datetime(x.endOfMute))
 	else:
 		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_Muted")
+		
 def decode_Muted(x):
 	return Muted(x[0], datetime.datetime(x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
 
 def save():
 	with open(MuteDataFilePath, "w") as write_file:
 		json.dump(muteList, write_file, default=encode_Muted, sort_keys=True, indent=2)
+		
 def load():
 	with open(MuteDataFilePath, "r") as read_file:
 		muteList = json.load(read_file, object_hook=decode_Muted)
 
+async def mute(bot, message):
+        try:
+                content = message.content
+                name = str(content[6:-3])
+                amount = int(content[-2:-1])
+                timeUnit = str(content[-1:])
+                if(timeUnit=='s'):
+                        m = Muted(name, datetime.datetime.now() + timedelta(seconds=amount))
+                        load()
+                        muteList.append(encode_Muted(m))
+                        save()
+                        await bot.send_message(message.channel, name+' has been muted for '+str(amount)+' second(s).')
+                elif(timeUnit=='m'):
+                        m = Muted(name, datetime.datetime.now() + timedelta(minutes=amount))
+                        load()
+                        muteList.append(encode_Muted(m))
+                        save()
+                        await bot.send_message(message.channel, name+' has been muted for '+str(amount)+' minute(s).')
+                elif(timeUnit=='h'):
+                        m = Muted(name, datetime.datetime.now() + timedelta(hours=amount))
+                        load()
+                        muteList.append(encode_Muted(m))
+                        save()
+                        await bot.send_message(message.channel, name+' has been muted for '+str(amount)+' hour(s).')
+                elif(timeUnit=='d'):
+                        m = Muted(name, datetime.datetime.now() + timedelta(days=amount))
+                        load()
+                        muteList.append(encode_Muted(m))
+                        save()
+                        await bot.send_message(message.channel, name+' has been muted for '+str(amount)+' day(s).')
+                else:
+                        raise ValueError
+        except:
+                await bot.send_message(message.channel, 'Invalid format.')
+"""		
 load()
 print (str(len(muteList)))
 for x in muteList:
 	print("user: {x.id}, " + str(x.endOfMute))
-
+"""
