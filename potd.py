e489818b7c28935fec7b2eedfb49cd36f8ca25d9
dolphingarlic <dolphingarlic@gmail.com>
2019-05-05 17:30:20 +0200

    Made potd.py and run.py prettier
    (frodakcin notice me pls)

--- potd.py
+++ potd.py
@@ -2,96 +2,113 @@ import json
 import discord
 from discord import *
 
-PotdDataFilePath = "potd.json"
+PotdDataFilePath = 'potd.json'
 contenderList = []
 maxPoints = 1000000
 
+
 class Contender:
-	def __init__(self, user, name, pts):
-		self.user = user
-		self.username = name
-		self.points = pts
+    def __init__(self, user, name, pts):
+        self.user = user
+        self.username = name
+        self.points = pts
 
-	def __lt__ (self, other):
-		if(self.points != other.points):
-			return self.points < other.points
-		return False
+    def __lt__(self, other):
+        if(self.points != other.points):
+            return self.points < other.points
+        return False
 
-	def __gt__ (self, other):
-		if(self.points != other.points):
-			return self.points > other.points
-		return False
+    def __gt__(self, other):
+        if(self.points != other.points):
+            return self.points > other.points
+        return False
 
-	def updatePoints(self, gained):
-		newPoints = min(self.points + gained,maxPoints)
-		if(newPoints<0):
-			newPoints = 0
-		self.points = newPoints
+    def updatePoints(self, gained):
+        newPoints = min(self.points + gained, maxPoints)
+        if(newPoints < 0):
+            newPoints = 0
+        self.points = newPoints
 
-	def toJSON(self):
-		return json.dumps(self, default=lambda o : o.__dict__, sort_keys=True, indent=4)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 
 def updateContender(x):
-	if isinstance(x, Contender):
-		for i in range(len(contenderList)):
-			if(contenderList[i].user == x.user):
-				x.updatePoints(contenderList[i].points)
-				del contenderList[i]
-				break
-		A = True
-		for i in range(len(contenderList)):
-			if(x > contenderList[i]):
-				contenderList.insert(i, x)
-				A = False
-				break
-		if(A):
-			contenderList.append(x)
-		save()
+    if isinstance(x, Contender):
+        for i in range(len(contenderList)):
+            if(contenderList[i].user == x.user):
+                x.updatePoints(contenderList[i].points)
+                del contenderList[i]
+                break
+        A = True
+        for i in range(len(contenderList)):
+            if(x > contenderList[i]):
+                contenderList.insert(i, x)
+                A = False
+                break
+        if(A):
+            contenderList.append(x)
+        save()
+
 
 async def getContenderList(bot, message):
-	e = Embed(title="POTD Leaderboard")
-	for i in range(10):
-		try:
-			e.add_field(name= str(i+1)+". " + str(contenderList[i].username), value = str(contenderList[i].points)+" points", inline=False)
-		except:
-			break
-	await bot.send_message(message.channel, embed=e)
+    e = Embed(title='POTD Leaderboard')
+    for i in range(10):
+        try:
+            e.add_field(name=f'str(i+1). {contenderList[i].username}',
+                        value=f'{contenderList[i].points} points',
+                        inline=False)
+        except:
+            break
+    await bot.send_message(message.channel, embed=e)
+
 
 async def getContenderData(bot, message):
-	name = message.mentions[0].id
-	for i in range(len(contenderList)):
-		if(contenderList[i].user == Contender(name,name,0).user):
-			await bot.send_message(message.channel, contenderList[i].username+" has "+str(contenderList[i].points)+" points for POTD")
+    name = message.mentions[0].id
+    for i in range(len(contenderList)):
+        if(contenderList[i].user == Contender(name, name, 0).user):
+            await bot.send_message(message.channel,
+                                   f'{contenderList[i].username} has {contenderList[i].points} points for POTD')
 
 
 async def updateLeaderboard(bot, message):
-	try:
-		name = message.mentions[0].id
-		nameToShow = message.mentions[0].display_name
-		try:
-			content=message.content[:message.content.index('pts')].strip()
-		except:
-			content=message.content.strip()
-		score = min(int(content.split()[-1]),maxPoints)
-		if(score<0):
-			score = 0
-		updateContender(Contender(name, nameToShow, score))
-	except:
-		pass
+    try:
+        name = message.mentions[0].id
+        nameToShow = message.mentions[0].display_name
+        try:
+            content = message.content[:message.content.index('pts')].strip()
+        except:
+            content = message.content.strip()
+        score = min(int(content.split()[-1]), maxPoints)
+        if(score < 0):
+            score = 0
+        updateContender(Contender(name, nameToShow, score))
+    except:
+        pass
 
-#START IO
+# START IO
+
+
 def encode_Contender(x):
-	if isinstance(x, Contender):
-		return {"user": (x.user), "username": (x.username), "pts": (x.points)}
-	else:
-		raise TypeError("Object of type {dt.__class__.__name__} is not compatible with encode_Contender")
+    if isinstance(x, Contender):
+        return {'user': (x.user), 'username': (x.username), 'pts': (x.points)}
+    else:
+        raise TypeError(
+            'Object of type {dt.__class__.__name__} is not compatible with encode_Contender')
+
+
 def decode_Contender(x):
-	return Contender(x["user"], x["username"], x["pts"])
+    return Contender(x['user'], x['username'], x['pts'])
+
+
 def save():
-	with open(PotdDataFilePath, "w") as write_file:
-		json.dump(contenderList, write_file, default=encode_Contender, sort_keys=False, indent=2)
+    with open(PotdDataFilePath, 'w') as write_file:
+        json.dump(contenderList, write_file,
+                  default=encode_Contender, sort_keys=False, indent=2)
+
+
 def load():
-	global contenderList
-	with open(PotdDataFilePath, "r") as read_file:
-		contenderList = json.load(read_file, object_hook=decode_Contender)
-#END IO
+    global contenderList
+    with open(PotdDataFilePath, 'r') as read_file:
+        contenderList = json.load(read_file, object_hook=decode_Contender)
+# END IO
--- run.py
+++ run.py
@@ -10,78 +10,83 @@ from potd import *
 
 prefix = '!'
 potdStatusChannelID = '518297095099121665'
-token = #remove when upload
+token = 'Yeet'  # remove when upload
+
+
 class MyClient(discord.Client):
-	async def on_ready(self):
-		print('Logged in as')
-		print(self.user.name)
-		print(self.user.id)
-		print('------')
-		raw = open(PotdDataFilePath, "w")
-		raw.write("[\n]")
-		raw.close()
-		messages = json.loads(requests.get("https://discordapp.com/api/v6/channels/" + potdStatusChannelID + "/messages", headers={"authorization": token}).text)
-		for i in messages:
-			var = i['content']
-			name = i['mentions'][0]['id']
-			nameToShow = i['mentions'][0]['username']
-			if('pts'in var):
-				content=var[:var.index('pts')].strip()
-			else:
-				content=var.strip()
-			score = min(int(content.split()[-1]),maxPoints)
-			if(score<0):
-				score = 0
-			updateContender(Contender(name, nameToShow, score))
-		load()
-		await self.change_presence(game=discord.Game(name="Tmw orz", url="https://codeforces.com/profile/tmwilliamlin168", type=0), status=Status.online, afk=False)
-	
-	async def on_message(self, message):
-		if message.author == self.user:
-			return
-		content = message.content
-		await updateMutes(self)	
-		
-		if censor.enabled:
-			if await censor.isCensored(content.lower()):
-				print("Message \"" + content + "\" from user " + message.author.name + " has been deleted.")
-				await self.delete_message(message)
-				return
-		if message.channel.id == potdStatusChannelID:
-			print("message recieved")
-			await updateLeaderboard(self, message)
-		
-		if content.startswith(prefix):
-			bot_command = True
-			content = content[len(prefix):]
-			if content.lower().startswith('mute '):
-				await mute(self, message)			
-			elif content.lower().startswith("mutelist"):
-				await getMuteList(self, message)
-			elif content.startswith('echo '):
-				await self.send_message(message.channel, content[5:])
-			elif content.startswith('8ball'):
-				await make_prediction(self, message)
-			elif content.lower().startswith('geniosity'):
-				await print_geniosity(self, message)
-			elif content.lower().startswith('leaderboard'):
-				await getContenderList(self, message)			
-			elif content.lower().startswith('points'):
-				await getContenderData(self, message)
-			elif content.startswith("censor "):
-				if censor.enabled:
-					await censor.censor_command(self, message.channel, content[7:])
-		else:
-			if 'geniosity' in content:
-				await react_geniosity(self, message)
-			if 'wtmoo' in content:
-				await react_wtmoo(self, message)
-			if 'orz' in content:
-				await react_orz(self, message)
-			if 'juicy' in content:
-				await react_juicy(self, message)
-			if 'tmw' in content:
-				await react_tmw(self, message)
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        raw = open(PotdDataFilePath, 'w')
+        raw.write('[\n]')
+        raw.close()
+        messages = json.loads(requests.get(
+            f'https://discordapp.com/api/v6/channels/{potdStatusChannelID}/messages', headers={'authorization': token}).text)
+        for i in messages:
+            var = i['content']
+            name = i['mentions'][0]['id']
+            nameToShow = i['mentions'][0]['username']
+            if 'pts'in var:
+                content = var[:var.index('pts')].strip()
+            else:
+                content = var.strip()
+            score = min(int(content.split()[-1]), maxPoints)
+            # if score < 0:
+            #     score = 0
+            updateContender(Contender(name, nameToShow, score))
+        load()
+        await self.change_presence(game=discord.Game(name='tmw orz', url='https://codeforces.com/profile/tmwilliamlin168', type=0), status=Status.online, afk=False)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        content = message.content
+        await updateMutes(self)
+
+        if censor.enabled:
+            if await censor.isCensored(content.lower()):
+                print(
+                    f'Message \'{content}\' from user {message.author.name} has been deleted.')
+                await self.delete_message(message)
+                return
+        if message.channel.id == potdStatusChannelID:
+            print('message recieved')
+            await updateLeaderboard(self, message)
+
+        if content.startswith(prefix):
+            bot_command = True
+            content = content[len(prefix):]
+            if content.lower().startswith('mute '):
+                await mute(self, message)
+            elif content.lower().startswith('mutelist '):
+                await getMuteList(self, message)
+            elif content.startswith('echo '):
+                await self.send_message(message.channel, content[5:])
+            elif content.startswith('8ball '):
+                await make_prediction(self, message)
+            elif content.lower().startswith('geniosity '):
+                await print_geniosity(self, message)
+            elif content.lower().startswith('leaderboard '):
+                await getContenderList(self, message)
+            elif content.lower().startswith('points '):
+                await getContenderData(self, message)
+            elif content.startswith('censor '):
+                if censor.enabled:
+                    await censor.censor_command(self, message.channel, content[7:])
+        else:
+            if 'geniosity' in content:
+                await react_geniosity(self, message)
+            if 'wtmoo' in content:
+                await react_wtmoo(self, message)
+            if 'orz' in content:
+                await react_orz(self, message)
+            if 'juicy' in content:
+                await react_juicy(self, message)
+            if 'tmw' in content:
+                await react_tmw(self, message)
+
 
 client = MyClient()
 client.run(token)
