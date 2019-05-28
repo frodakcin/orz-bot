38424f021586d5025b57ff735bd6fdc9e804ecc8
Blastman <48421287+aav123@users.noreply.github.com>
2019-05-28 00:21:32 -0400

    Update geniosity.py

--- geniosity.py
+++ geniosity.py
@@ -1,28 +1,43 @@
-import asyncio
-from discord import *
 from discord.utils import *
 
+
 async def print_geniosity(bot, message):
-	await bot.send_message(message.channel, '<:geniosity:554459617271480321>')
+    await bot.send_message(message.channel, '<:geniosity:554459617271480321>')
+
 
 async def react_geniosity(bot, message):
-	emoji = get(bot.get_all_emojis(), name='geniosity')
-	await bot.add_reaction(message, emoji)
+    emoji = get(bot.get_all_emojis(), name='geniosity')
+    await bot.add_reaction(message, emoji)
+
 
 async def react_wtmoo(bot, message):
-	emoji = get(bot.get_all_emojis(), name='wtmoo')
-	await bot.add_reaction(message, emoji)
+    emoji = get(bot.get_all_emojis(), name='wtmoo')
+    await bot.add_reaction(message, emoji)
+
+
+async def react_headpat(bot, message):
+    emoji = get(bot.get_all_emojis(), name='blobpat')
+    await bot.add_reaction(message, emoji)
+
+async def react_egg(bot, message):
+    await bot.add_reaction(message, "🥚")
+
+async def react_ship(bot, message):
+    await bot.add_reaction(message, "❤")
+    await bot.add_reaction(message, "🚢")
 
 async def react_orz(bot, message):
-	emoji = get(bot.get_all_emojis(), name='orz')
-	await bot.add_reaction(message, emoji)
+    emoji = get(bot.get_all_emojis(), name='orz')
+    await bot.add_reaction(message, emoji)
+
 
 async def react_juicy(bot, message):
-	emoji = get(bot.get_all_emojis(), name='juicy')
-	await bot.add_reaction(message, emoji)
+    emoji = get(bot.get_all_emojis(), name='juicy')
+    await bot.add_reaction(message, emoji)
+
 
 async def react_tmw(bot, message):
-	await bot.add_reaction(message, '🇹')
-	await bot.add_reaction(message,'🇲')
-	await bot.add_reaction(message, '🇼')
-	await bot.add_reaction(message, '❤')
+    await bot.add_reaction(message, '🇹')
+    await bot.add_reaction(message, '🇲')
+    await bot.add_reaction(message, '🇼')
+    await bot.add_reaction(message, '❤')
