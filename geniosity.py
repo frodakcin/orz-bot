6a027fa60bc99c2ca89579f57f56d8333a6860d2
Blastman <48421287+aav123@users.noreply.github.com>
2020-01-18 00:57:58 -0500

    Update geniosity.py

--- geniosity.py
+++ geniosity.py
@@ -2,46 +2,39 @@ from discord.utils import *
 
 
 async def print_geniosity(bot, message):
-	await bot.send_message(message.channel, '<:geniosity:554459617271480321>')
+	await message.channel.send('<:geniosity:554459617271480321>')
 
 
-async def react_geniosity(bot, message):
-	emoji = get(bot.get_all_emojis(), name='geniosity')
-	await bot.add_reaction(message, emoji)
+async def react_geniosity(bot, message):    
+	await message.add_reaction('<:geniosity:516424110205566987>')
 
 
 async def react_wtmoo(bot, message):
-	emoji = get(bot.get_all_emojis(), name='wtmoo')
-	await bot.add_reaction(message, emoji)
+	await message.add_reaction('<:wtmoo:526363524276944917>')
 
 
 async def react_headpat(bot, message):
-	emoji = get(bot.get_all_emojis(), name='blobpat')
-	await bot.add_reaction(message, emoji)
+	await message.add_reaction('<:blobpat:569680182567370762>')
 
 async def react_ayaya(bot, message):
-	emoji = get(bot.get_all_emojis(), name='ayaya')
-	await bot.add_reaction(message, emoji)
+	await message.add_reaction('<:ayaya:596997427870040064>')
 
 async def react_egg(bot, message):
-	await bot.add_reaction(message, "🥚")
+	await message.add_reaction('🥚')
 
 async def react_ship(bot, message):
-	await bot.add_reaction(message, "❤")
-	await bot.add_reaction(message, "🚢")
+	await message.add_reaction('❤')
+	await message.add_reaction('🚢')
 
 async def react_orz(bot, message):
-	emoji = get(bot.get_all_emojis(), name='orz')
-	await bot.add_reaction(message, emoji)
-
+	await message.add_reaction('<:orz:545130316067635200>')
 
 async def react_juicy(bot, message):
-	emoji = get(bot.get_all_emojis(), name='juicy')
-	await bot.add_reaction(message, emoji)
+	await message.add_reaction('<:juicy:545051411692650536>')
 
 
 async def react_tmw(bot, message):
-	await bot.add_reaction(message, '🇹')
-	await bot.add_reaction(message, '🇲')
-	await bot.add_reaction(message, '🇼')
-	await bot.add_reaction(message, '❤')
+	await message.add_reaction('🇹')
+	await message.add_reaction('🇲')
+	await message.add_reaction('🇼')
+	await message.add_reaction('❤')
