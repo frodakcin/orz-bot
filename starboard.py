62fe251d7cce10409df52a6789993e43d10aff21
Blastman <48421287+aav123@users.noreply.github.com>
2020-07-17 01:48:08 -0400

    Update starboard.py

--- starboard.py
+++ starboard.py
@@ -1,29 +1,54 @@
 from discord import *
 import asyncio 
 
-STAR_LIMIT = 3
-STARBOARD = 577381775739846676
+LIMIT = 4
+GALLERY = 577381775739846676
 STAR = '⭐'
-STARRED_MSGS = set()
+GENIOSITY = '<:geniosity:516424110205566987>'
+MSGS = set()
 
-async def post_embed(bot, message):
-    if message.id not in STARRED_MSGS:
-        embed = make_embed(message)
-        await bot.get_channel(STARBOARD).send(embed=embed)
-        STARRED_MSGS.add(message.id)
+async def post_star(bot, message):
+    if message.id not in MSGS:
+        embed = make_star(message)
+        await bot.get_channel(GALLERY).send(embed=embed)
+        MSGS.add(message.id)
+
+async def post_geniosity(bot, message):
+    if message.id not in MSGS:
+        embed = make_geniosity(message)
+        await bot.get_channel(GALLERY).send(embed=embed)
+        MSGS.add(message.id)
 
 
-def make_embed(message):
-        embed = Embed()
+def make_star(message):
+    
+        if(len(message.content) > 256):
+            embed = Embed(title =  message.content[:250] + " . . .", description = "[Jump to message](" + message.jump_url + ")", color = 0xffac33)
+        else:
+            embed = Embed(title =  message.content, description = "[Jump to message](" + message.jump_url + ")", color = 0xffac33)
 
-        if message.content:
-            embed.add_field(name = 'message', value = message.content, inline = False)
+        embed.set_author(name = str(message.author), icon_url = message.author.avatar_url)
 
         if message.attachments:
             file = message.attachments[0]
-            if str(file.url).endswith(('jpg', 'jpeg', 'png')):
+            if str(file.url).lower().endswith(("jpg", "jpeg", "png")):
                 embed.set_image(url = file.url)
 
-        embed.add_field(name = 'link', value = '[click me](' + message.jump_url + ')')
-        embed.set_footer(text = str(message.author), icon_url = message.author.avatar_url)
+        return embed
+
+
+def make_geniosity(message):
+
+        if(len(message.content) > 256):
+            embed = Embed(title =  message.content[:250] + " . . .", description = "[Jump to message](" + message.jump_url + ")", color = 0x2ecc71)
+        else:
+            embed = Embed(title =  message.content, description = "[Jump to message](" + message.jump_url + ")", color = 0x2ecc71)
+
+        embed.set_author(name = str(message.author), icon_url = message.author.avatar_url)
+
+        if message.attachments:
+            file = message.attachments[0]
+            if str(file.url).lower().endswith(("jpg", "jpeg", "png")):
+                embed.set_image(url = file.url)
+
         return embed
